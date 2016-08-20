7e1d2f29cfdc9c8b29c618a61bf6da4753c24d4d
Brandon Truter <brandontruter_it@ymail.com>
2016-08-20 20:32:16 +0200

    installed dependencies, first run in browser

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    selector: 'tangent-app',
+    template: '<h1>My First Angular 2 App</h1><hr/><h4>Time for some authentication ....</h4><hr/>'
 })
 export class AppComponent { }
